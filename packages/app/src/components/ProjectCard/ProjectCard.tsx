21e4ebbca15190f3c39bf3061742c53cfb017272
Taiju Sanagi <taijusanagi@Taijus-MacBook-Pro.local>
2023-04-22 10:05:45 +0800

    feat: top page ui

---
+++ packages/app/src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export interface ProjectCardProps {
+  thumbnail: string;
+  id: string;
+  title: string;
+  shortDescription: string;
+}
+
+export const ProjectCard = ({ thumbnail, id, title, shortDescription }: ProjectCardProps) => {
+  return (
+    <a
+      href={`http://localhost:3000/${id}`}
+      className="group flex flex-col w-64 rounded-lg border border-transparent p-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 overflow-hidden"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="w-full h-40 overflow-hidden">
+        <img src={thumbnail} alt="Thumbnail" className="w-full h-full object-cover" />
+      </div>
+      <div className="mt-4">
+        <h2 className="mb-2 text-xl font-semibold">{title}</h2>
+        <p className="m-0 text-sm">{shortDescription}</p>
+      </div>
+    </a>
+  );
+};
---
+++ packages/app/src/data/index.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    thumbnail: "https://via.placeholder.com/400",
+    banner: "https://via.placeholder.com/800x450",
+    id: "1",
+    title: "ETHTokyo",
+    shortDescription: "Experience the vibrant Ethereum community in Tokyo.",
+    longDescription:
+      "ETHTokyo is a hackathon event where developers, designers, and enthusiasts come together to push the boundaries of the Ethereum ecosystem. Participants can collaborate, learn from industry experts, and create innovative decentralized applications. Join us for an unforgettable experience!",
+  },
+  {
+    thumbnail: "https://via.placeholder.com/400",
+    banner: "https://via.placeholder.com/800x450",
+    id: "2",
+    title: "ETHTaipei",
+    shortDescription: "Dive into the world of Ethereum in Taipei.",
+    longDescription:
+      "ETHTaipei is an immersive hackathon that brings together Ethereum enthusiasts, developers, and industry leaders. Participants will work together to build cutting-edge decentralized applications while learning from experienced mentors. Don't miss this opportunity to be a part of the growing Ethereum community in Taipei!",
+  },
+  {
+    thumbnail: "https://via.placeholder.com/400",
+    banner: "https://via.placeholder.com/800x450",
+    id: "3",
+    title: "ETHLisbon",
+    shortDescription: "Discover the future of Ethereum in Lisbon.",
+    longDescription:
+      "ETHLisbon is a premier hackathon event focused on the Ethereum ecosystem. Developers, designers, and blockchain enthusiasts will come together to create innovative solutions and explore the latest developments in decentralized technology. Join us for an exciting weekend of learning, networking, and innovation!",
+  },
+  {
+    thumbnail: "https://via.placeholder.com/400",
+    banner: "https://via.placeholder.com/800x450",
+    id: "4",
+    title: "ETHOnline",
+    shortDescription: "Join the global Ethereum hackathon from anywhere.",
+    longDescription:
+      "ETHOnline is a virtual hackathon that brings together participants from around the world to collaborate on Ethereum projects. With mentorship from industry experts and access to the latest tools and resources, you'll be able to create groundbreaking decentralized applications from the comfort of your own home. Don't miss this chance to connect with the global Ethereum community!",
+  },
+];
