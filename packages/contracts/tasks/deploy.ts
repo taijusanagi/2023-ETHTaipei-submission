9d7cab06a49e8c98c51be0365fb8cdd1ceb62647
Taiju Sanagi <taijusanagi@Taijus-MacBook-Pro.local>
2023-04-22 22:19:05 +0800

    feat: deploy contract script

--- packages/contracts/tasks/deploy.ts
+++ packages/contracts/tasks/deploy.ts
@@ -2,24 +2,27 @@ import { task, types } from "hardhat/config";
 
 task("deploy", "Deploy a Feedback contract")
   .addOptionalParam("semaphore", "Semaphore contract address", undefined, types.string)
-  .addOptionalParam("group", "Group id", "42", types.string)
+  .addOptionalParam("xpoap", "XPOAP contract address", undefined, types.string)
   .addOptionalParam("logs", "Print the logs", true, types.boolean)
-  .setAction(async ({ logs, semaphore: semaphoreAddress, group: groupId }, { ethers, run }) => {
+  .setAction(async ({ logs, semaphore: semaphoreAddress, xpoap: xpoapAddress }, { ethers, run }) => {
     if (!semaphoreAddress) {
       const { semaphore } = await run("deploy:semaphore", {
         logs,
       });
-
       semaphoreAddress = semaphore.address;
     }
 
-    if (!groupId) {
-      groupId = process.env.GROUP_ID;
+    if (!xpoapAddress) {
+      const MockXPOAP = await ethers.getContractFactory("MockXPOAP");
+      const xpoapContract = await MockXPOAP.deploy();
+      xpoapAddress = xpoapContract.address;
+      if (logs) {
+        console.log("XPOAP has been deployed to:", xpoapAddress);
+      }
     }
 
-    const FeedbackFactory = await ethers.getContractFactory("Feedback");
-
-    const feedbackContract = await FeedbackFactory.deploy(semaphoreAddress, groupId);
+    const VerifiableAnonymous = await ethers.getContractFactory("VerifiableAnonymous");
+    const feedbackContract = await VerifiableAnonymous.deploy(semaphoreAddress, xpoapAddress);
 
     await feedbackContract.deployed();
 
